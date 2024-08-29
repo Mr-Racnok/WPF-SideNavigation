3ad822484998e3472cbfa601ca29cc84238f8aa1
Jougo Cahya Wirana <jugowirana1@gmail.com>
2024-08-29 09:13:11 +0700

    Add project files.

---
+++ SideNavigation/MainWindow.xaml.vb
@@ -0,0 +1,19 @@
+﻿Class MainWindow
+
+    Private Sub Border_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        If e.LeftButton = MouseButtonState.Pressed Then
+            Me.DragMove()
+        End If
+    End Sub
+
+    Private Sub Border_MouseLeftButtonDown(sender As Object, e As MouseButtonEventArgs)
+        If e.ClickCount = 2 Then
+            If Me.WindowState = WindowState.Maximized Then
+                Me.WindowState = WindowState.Normal
+            Else
+                Me.WindowState = WindowState.Maximized
+            End If
+        End If
+    End Sub
+
+End Class
